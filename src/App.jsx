f18d4fd738f532824149a6f446f3d716892cce46
Alex-Alessi <alex.alessi246@gmail.com>
2025-04-04 10:47:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const products = [
+  { name: "Mela", price: 0.5 },
+  { name: "Pane", price: 1.2 },
+  { name: "Latte", price: 1.0 },
+  { name: "Pasta", price: 0.7 },
+];
+
+function App() {
+  return (
+    <>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.name + ", " + product.price}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
