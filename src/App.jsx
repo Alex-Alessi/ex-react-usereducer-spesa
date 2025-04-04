3473a200c29f1841ff1bf8250e2876ea9af23e42
Alex-Alessi <alex.alessi246@gmail.com>
2025-04-04 17:14:19 +0200

    Milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,37 @@ function App() {
 
   const addToCart = (product) => {
     const isProduct = (p) => p.name === product.name;
-    if (addedProducts.some(isProduct)) return;
     const ShallowCopyProduct = { ...product, quantity: 1 };
-    setAddedProducts((curr) => [...curr, ShallowCopyProduct]);
+    if (addedProducts.some(isProduct)) {
+      const modifyArr = addedProducts.map((e) => {
+        return e.name === product.name ? { ...e, quantity: e.quantity + 1 } : e;
+      });
+      setAddedProducts(modifyArr);
+    } else {
+      setAddedProducts((curr) => [...curr, ShallowCopyProduct]);
+    }
+  };
+
+  const removeFromCart = (product) => {
+    if (product.quantity > 1) {
+      const decreaseQuantity = addedProducts.map((obj) => {
+        return obj.name === product.name
+          ? { ...obj, quantity: obj.quantity - 1 }
+          : obj;
+      });
+      setAddedProducts(decreaseQuantity);
+    } else {
+      const modifyArr = addedProducts.filter((e) => {
+        return e.name != product.name;
+      });
+      setAddedProducts(modifyArr);
+    }
+  };
+
+  const calcola = () => {
+    let valore = 0;
+    addedProducts.forEach((e) => (valore += e.price * e.quantity));
+    return valore.toFixed(2);
   };
 
   return (
@@ -28,7 +56,7 @@ function App() {
           <li key={index}>
             {product.name}, {product.price}
             <button onClick={() => addToCart(product)}>
-              "Aggiungi al carrello
+              Aggiungi al carrello
             </button>
           </li>
         ))}
@@ -39,9 +67,13 @@ function App() {
           <li key={index}>
             {product.name}({product.price} €),{" "}
             {"quantity = " + product.quantity}
+            <button onClick={() => removeFromCart(product)}>
+              Rimuovi dal carrello
+            </button>
           </li>
         ))}
       </ul>
+      <h2>Totale da pagare: {calcola()} €</h2>
     </>
   );
 }
