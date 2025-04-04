8a4d3c6b3252e7dbddca8459ae345287ea824d09
Alex-Alessi <alex.alessi246@gmail.com>
2025-04-04 10:53:42 +0200

    milestone 1 products in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-const products = [
-  { name: "Mela", price: 0.5 },
-  { name: "Pane", price: 1.2 },
-  { name: "Latte", price: 1.0 },
-  { name: "Pasta", price: 0.7 },
-];
-
 function App() {
+  const products = [
+    { name: "Mela", price: 0.5 },
+    { name: "Pane", price: 1.2 },
+    { name: "Latte", price: 1.0 },
+    { name: "Pasta", price: 0.7 },
+  ];
   return (
     <>
       <ul>
